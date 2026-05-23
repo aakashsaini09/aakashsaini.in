81120feacfcecb51fb04eb0ddfd2a5c1e6e83b55
aakashsaini09 <aakashsaini948585@gmail.com>
2026-05-23 15:55:30 +0530

    resume fix

--- components/floating-dock-demo.tsx
+++ components/floating-dock-demo.tsx
@@ -43,7 +43,7 @@ export default function FloatingDockDemo() {
       icon: (
         <FaFilePdf className="h-full w-full text-neutral-500 dark:text-neutral-300" target="_blank"/>
       ),
-      href: "https://drive.google.com/file/d/1eYmxcPSqOQQSQhYGS6Jh94akawrg4LpH/view?usp=sharing",
+      href: "https://drive.google.com/file/d/1LELrJesB3L6YSfQoD-o0MO--z0miYFA8/view?usp=sharing",
     },
     {
       title: "Twitter",
